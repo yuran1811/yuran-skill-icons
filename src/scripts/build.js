492e02865c3a69efc2886156242373639a3d5ff8
yuran1811 <trieuvanbd123@gmail.com>
2025-02-12 13:50:54 +0700

    build: 📦 update build icons-json script behaviour

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,18 +1,21 @@
-import fs from "fs";
+import fs from "node:fs";
+import path from "node:path";
 
 const ICONS_PATH = "./public/icons";
 
-const iconsDir = fs.readdirSync(ICONS_PATH);
 const icons = {};
+const iconsDir = fs.readdirSync(ICONS_PATH);
+
 for (const icon of iconsDir) {
   if (icon.endsWith(".json")) continue;
 
+  const iconPath = path.join(ICONS_PATH, icon);
   const name = icon.replace(".svg", "").toLowerCase();
+
+  fs.renameSync(iconPath, `${ICONS_PATH}/${name}.svg`);
   icons[name] = String(fs.readFileSync(`${ICONS_PATH}/${icon}`));
 }
 
-// if (!fs.existsSync('./dist')) fs.mkdirSync('./dist');
-// fs.writeFileSync('./dist/icons.json', JSON.stringify(icons));
 fs.writeFileSync(`${ICONS_PATH}/icons.json`, JSON.stringify(icons), {
   flag: "w",
 });
